34c46cb1a0e37ec88c0f5f435e7bebd04918f7dc
lolovski <alexpaxyaev@gmail.com>
2024-10-17 22:01:16 +0300

    tsk_money

--- utils/mailing.py
+++ utils/mailing.py
@@ -8,9 +8,12 @@ from keyboard.inline.task import task_acceptance
 async def mailing_task(bot: Bot, task: Task):
     users = await get_users()
     for user in users:
-        await bot.send_message(user.tg_id,
-                               text=f"<b>{task.title} | {task.reward}₽\n\n"
-                                      f"📋{task.text}\n\n"
-                                      f"⚒️{task.category.name}</b>",
-                               reply_markup=await task_acceptance(task_id=task.id)
-                               )
+        try:
+            await bot.send_message(user.tg_id,
+                                   text=f"<b>{task.title} | {task.reward}₽\n\n"
+                                          f"📋{task.text}\n\n"
+                                          f"⚒️{task.category.name}</b>",
+                                   reply_markup=await task_acceptance(task_id=task.id)
+                                   )
+        except:
+            ...
