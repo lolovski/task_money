0732459b0bbf81f0622d36da2686fc684fcc21fe
lolovski <alexpaxyaev@gmail.com>
2025-04-19 16:04:50 +0300

    tsk_money

--- main.py
+++ main.py
@@ -47,5 +47,5 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    #logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
